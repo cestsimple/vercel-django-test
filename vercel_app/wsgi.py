1bc28876748ab14564f98badbae4efd6a7433d7f
hoowoo <hoowoo@redmi.book>
2023-04-04 21:31:39 +0800

    change django version to 4.1.3

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for vercel project.
+WSGI config for vercel_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
 app = get_wsgi_application()
